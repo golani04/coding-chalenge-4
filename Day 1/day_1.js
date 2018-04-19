849fa053b5aba45e0ee1699188f8485230fa0b19
Leonid Spivak <golani04@gmail.com>
2018-04-19 17:00:01 +0300

    Day 1 santa's floor

---
+++ Day 1/day_1.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+
+
+fs.readFile('./input.txt', (err, data) => {
+  console.time('chalenge#4');
+  // escape, if errored
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const whichFloor = { '(': 1, ')': -1 };
+  
+  data = data.toString().split('');
+  let basement = null;
+
+  const finalFloor = data.reduce((floor, whereToGo, index) => {
+    // second chalenge
+    if (floor === -1 && basement === null) {
+      basement = index;
+    }
+    // first chalenge
+    return floor += (whichFloor[whereToGo] || 0);
+  }, 0);
+
+  console.timeEnd('chalenge#4');
+});
