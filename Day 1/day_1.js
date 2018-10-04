ac1e6d392df485d81ede8e41ea33ef0913708f3f
Leonid Spivak <golani04@gmail.com>
2018-10-04 21:49:38 +0300

    updated if

--- Day 1/day_1.js
+++ Day 1/day_1.js
@@ -10,18 +10,18 @@ fs.readFile('./input.txt', (err, data) => {
     return;
   }
   const whichFloor = { '(': 1, ')': -1 };
-  
+
   data = data.toString().split('');
   let basement = null;
 
   const finalFloor = data.reduce((floor, whereToGo, index) => {
     // second chalenge
-    if (floor === -1 && basement === null) {
+    if (floor < 0 && basement === null) {
       basement = index;
     }
     // first chalenge
     return floor += (whichFloor[whereToGo] || 0);
   }, 0);
-
+  console.log(finalFloor, basement);
   console.timeEnd('chalenge#4');
 });
